480ece0e78c1b22a2c26c8836cf8a159683396bf
triketra <triketra@yandex.ru>
2022-09-11 16:37:49 +0800

    dz4

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int [] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 33);
+    Console.Write(" " + Method (i) + " ");
+ }
+
+int Method (int a)
+{
+    return numbers[a];
+}
