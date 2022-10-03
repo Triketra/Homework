a295f4d2e36e5de892077a6b8f702bf9f084fb5b
triketra <triketra@yandex.ru>
2022-10-04 01:03:51 +0800

    seminar8

---
+++ Zadacha_56/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int temp = int.MaxValue;
+int sum = 0;
+int k = 0;
+int [,] matrix = new int[rows, columns];
+
+
+if(rows != columns)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine(" ");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum += matrix[i,j];
+    }
+    Console.WriteLine($"Сумма элементов строки {i+1}: {sum}");
+    if (temp > sum)
+    {
+        temp = sum;
+        k = i;
+    }
+}
+ Console.WriteLine($"Номер строки с наименьшим количеством элементов: {k+1}");
+}
+
+else
+{
+    Console.WriteLine("Массив не прямоугольный");
+}
---
+++ Zadacha_58/Program.cs
@@ -0,0 +1,60 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.WriteLine("Введите число строк 1-й матрицы: ");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов 1-й матрицы: ");
+int columns1 = Convert.ToInt32(Console.ReadLine());
+
+int rows2 = columns1;
+int columns2 = rows1;
+
+Console.WriteLine("Первая матрица: ");
+int[,] matrix = new int [rows1, columns1];
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(-50, 51);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+Console.WriteLine("Вторая матрица: ");
+int[,] matrix2 = new int [rows2, columns2];
+
+for(int i = 0; i < matrix2.GetLength(1); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix2[i,j] = new Random().Next(-50,51);
+        Console.Write(matrix2[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(" ");
+Console.WriteLine("Произведение матриц: ");
+
+int[,] matrixAnsver = new int[matrix.GetLength(0), matrix2.GetLength(1)];
+for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+    for(int j = 0; j <matrix2.GetLength(1); j++)
+    {
+        for (int k=0; k<matrix2.GetLength(0); k++)
+        {
+            matrixAnsver[i,j] +=matrix[i,k] * matrix2[k,j];
+        }
+        Console.Write(matrixAnsver[i,j] + "\t");
+    }
+    Console.WriteLine();
+    }
+    
